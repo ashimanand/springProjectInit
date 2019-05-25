c330e3b46250e09cb1cf300e79437592590f2d7b
ashim anand <ashimanand@ashims-MacBook-Air.local>
2019-05-25 17:17:06 +0530

    initial project structure

---
+++ projectInit/src/main/java/com/anand/projectInit/ProjectInitApplication.java
@@ -0,0 +1,13 @@
+package com.anand.projectInit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectInitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectInitApplication.class, args);
+	}
+
+}
